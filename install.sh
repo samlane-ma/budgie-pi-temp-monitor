41baed56f71a9307aa9b3efde3cd942f970a19b5
Sam Lane <67085765+samlane-ma@users.noreply.github.com>
2020-07-16 21:16:21 -0400

    New install script for schema
    Added new instructions to install the schema

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ sudo mkdir -p /usr/lib/budgie-desktop/plugins/budgie-pi-temp-monitor
 sudo cp budgie_pi_temp_monitor.py /usr/lib/budgie-desktop/plugins/budgie-pi-temp-monitor
 sudo cp BudgiePiTempMonitor.plugin /usr/lib/budgie-desktop/plugins/budgie-pi-temp-monitor
 sudo cp -n sensors-temperature-symbolic.svg /usr/share/pixmaps/
+sudo cp com.github.samlane-ma.budgie-pi-temp-monitor.gschema.xml /usr/share/glib-2.0/schemas
+sudo sudo glib-compile-schemas /usr/share/glib-2.0/schemas
