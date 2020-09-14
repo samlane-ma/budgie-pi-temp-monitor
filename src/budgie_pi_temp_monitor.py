2e0d77bf1cc54ae7b2ecd80792d40d9d96a633f8
Sam Lane <67085765+samlane-ma@users.noreply.github.com>
2020-09-13 23:44:52 -0400

    Initial commit of added features
    Applet now shows highest and lowest temperature in popover
    Icon now will be normally dim and get bright when temperature approaches throttle limit of 85 degrees (will get brighter at 75+)

--- src/budgie_pi_temp_monitor.py
+++ src/budgie_pi_temp_monitor.py
@@ -4,11 +4,14 @@ import os.path
 import subprocess
 import time
 import threading
+from datetime import datetime
 gi.require_version('Budgie', '1.0')
 from gi.repository import Budgie, GObject, Gtk, Gio, GLib
 
 """
-    Budgie Pi Temperature Monitor Plugin - Show the Current CPU Temp on Raspberry Pi
+    Budgie Pi Temperature Monitor Plugin
+    Show the Current CPU Temp on Raspberry Pi
+
     Copyright (C) 2020  Samuel Lane
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -20,13 +23,37 @@ from gi.repository import Budgie, GObject, Gtk, Gio, GLib
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-    
+
     Icons made by Freepik from http://www.freepik.com
     https://www.flaticon.com
 """
 
+HI_TEMP = 1
+HI_TIME = 2
+LOW_TEMP = 5
+LOW_TIME = 6
+
 app_settings = Gio.Settings.new("com.github.samlane-ma.budgie-pi-temp-monitor")
 path = "com.solus-project.budgie-panel"
+
+class PiTemp:
+
+    def __init__(self, _temp = 0, _time = datetime.now()):
+        self._temp = _temp
+        self._time = _time
+
+    def get_temp(self, celcius=True):
+        ret_temp = self._temp / 1000
+        if celcius:
+            degree = "°C"
+        else:
+            ret_temp = ret_temp * 9 / 5 + 32
+            degree = "°F"
+        return format(ret_temp, '.2f') + degree
+
+    def get_time(self):
+        return self._time.strftime("%x %X")
+
 
 class BudgiePiTemp(GObject.GObject, Budgie.Plugin):
     """ This is simply an entry point into your Budgie Applet implementation.
@@ -86,20 +113,41 @@ class BudgiePiTempApplet(Budgie.Applet):
         Budgie.Applet.__init__(self)
 
         self.uuid = uuid
-        self.connect("destroy",Gtk.main_quit)
         self.keep_running = True
-
-        self.currenttemp = "--"
+        self.overheat = False
+        self.label_names = ["Highest Temp","","","","Lowest Temp:","",""]
+        self.currenttemp = ""
         self.sleeptime = app_settings.get_int("interval")
         self.celcius = app_settings.get_boolean("celcius")
 
-        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        self.add(box)
+        self.panel_box = Gtk.EventBox()
+        self.box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        self.panel_box.add(self.box)
+        self.add(self.panel_box)
         self.cputemp = Gtk.Label(self.currenttemp+"°C")
         self.temp_icon = Gtk.Image.new_from_icon_name("sensors-temperature-symbolic",Gtk.IconSize.MENU)
-        box.pack_start(self.cputemp, expand = True, fill = True, padding = 0)
-        box.pack_end(self.temp_icon, expand = True, fill = True, padding = 0)
+        self.box.pack_start(self.cputemp, expand = True, fill = True, padding = 0)
+        self.box.pack_end(self.temp_icon, expand = True, fill = True, padding = 0)
+        self.temp_icon.get_style_context().add_class("dim-label")
+        self.menu_box = Gtk.Grid()
+        self.labels = []
+        for n in range(7):
+            label = Gtk.Label(label=self.label_names[n])
+            self.labels.append(label)
+            self.menu_box.attach(self.labels[n],0,n,1,1)
+        self.popover = Budgie.Popover.new(self.panel_box)
+        self.popover.add(self.menu_box)
+        self.panel_box.connect("button-press-event", self.on_press)
+        self.menu_box.show_all()
         self.show_all()
+        self.show_all()
+
+        self.hi_temp = PiTemp(0)
+        self.low_temp = PiTemp(1000000)
+        self.labels[HI_TEMP].set_text(self.hi_temp.get_temp(self.celcius))
+        self.labels[HI_TIME].set_text(self.hi_temp.get_time())
+        self.labels[LOW_TEMP].set_text(self.low_temp.get_temp(self.celcius))
+        self.labels[LOW_TIME].set_text(self.low_temp.get_time())
 
         app_settings.connect("changed",self.load_settings)
 
@@ -113,6 +161,10 @@ class BudgiePiTempApplet(Budgie.Applet):
     def load_settings(self, *args):
         self.sleeptime = app_settings.get_int("interval")
         self.celcius = app_settings.get_boolean("celcius")
+        self.labels[HI_TEMP].set_text(self.hi_temp.get_temp(self.celcius))
+        self.labels[HI_TIME].set_text(self.hi_temp.get_time())
+        self.labels[LOW_TEMP].set_text(self.low_temp.get_temp(self.celcius))
+        self.labels[LOW_TIME].set_text(self.low_temp.get_time())
         GObject.idle_add(self.update_panel)
 
     def update_temp(self):
@@ -124,11 +176,28 @@ class BudgiePiTempApplet(Budgie.Applet):
             time.sleep(self.sleeptime)
 
     def update_panel(self):
-        temp_int = int(self.currenttemp) / 1000
+        temp_int = int(self.currenttemp)
+        if self.overheat and temp_int < 75000:
+            self.temp_icon.get_style_context().add_class("dim-label")
+            self.overheat = False
+        elif not self.overheat and temp_int >= 75000:
+            self.temp_icon.get_style_context().remove_class("dim-label")
+            self.overheat = True
+        if temp_int > self.hi_temp._temp:
+            self.hi_temp._temp = temp_int
+            self.hi_temp._time = datetime.now()
+            self.labels[HI_TEMP].set_text(self.hi_temp.get_temp(self.celcius))
+            self.labels[HI_TIME].set_text(self.hi_temp.get_time())
+        if temp_int < self.low_temp._temp:
+            self.low_temp._temp = temp_int
+            self.low_temp._time = datetime.now()
+            self.labels[LOW_TEMP].set_text(self.low_temp.get_temp(self.celcius))
+            self.labels[LOW_TIME].set_text(self.low_temp.get_time())
         if not self.celcius:
             suffix = "°F"
-            temp_int = temp_int * 9 / 5 + 32
+            temp_int = temp_int * 9 / 5 / 1000 + 32
         else:
+            temp_int = temp_int / 1000
             suffix = "°C"
         temp = str(round(temp_int))
         self.cputemp.set_text(temp + suffix)
@@ -159,6 +228,14 @@ class BudgiePiTempApplet(Budgie.Applet):
             # Disconnect the signals
             self.currpanelsubject_settings.disconnect(self.panel_signal_id)
             self.keep_running = False
+
+    def on_press(self, panel_box, event):
+        if event.button == 1:
+            self.manager.show_popover(self.panel_box)
+
+    def do_update_popovers(self, manager):
+        self.manager = manager
+        self.manager.register_popover(self.panel_box, self.popover)
 
     def do_supports_settings(self):
         """Return True if support setting through Budgie Setting,
